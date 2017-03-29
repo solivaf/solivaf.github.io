dcbf432818c3414d77a0b94d550e321eac17a703
Luis Soliva <lfernando.soliva@gmail.com>
2017-03-28 23:11:50 -0300

    Auto-generated commit

--- src/app/model/post.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Created by luissoliva on 3/28/17.
- */
-export class Post {
-
-  constructor(public title, public subtitle, public author, public createdAt, public content, public img) {}
-
-}
